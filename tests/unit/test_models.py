70e6c118266c6466b5cb9897e6c2cefc5973f2f1
PatrykB-dot <patrykfor3@gmail.com>
2022-01-10 19:57:08 +0100

    Added asset test

--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -1,4 +1,4 @@
-from app.models import User
+from app.models import User, Asset
 
 
 def test_new_user():
@@ -7,3 +7,10 @@ def test_new_user():
     assert user.email == "patryk@test.pl"
     user.set_password("Testing Password")
     assert user.password_hash != "Testing Password"
+
+def test_new_asset():
+    asset = Asset(name='Asus', type='pc', owner='test', serial_number='12345678')
+    assert asset.name == 'Asus'
+    assert asset.type == 'pc'
+    assert asset.owner == 'test'
+    assert asset.serial_number != '09876'
