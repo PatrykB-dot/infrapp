9fb47d6b0f10ec8348ea9d913f0284654c0c9d67
PatrykB-dot <patrykfor3@gmail.com>
2022-01-10 20:38:17 +0100

    Update __init.__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import os
+from config import Config
 
 app=Flask(__name__)
 app.config.from_object(Config)
@@ -13,4 +15,23 @@ if __name__ == "__main__":
 login = LoginManager(app)
 login.login_view = 'login'
 
-from app import routes,models
+#from app import routes,models
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_pyfile('config.py')
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        pass
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    return app
