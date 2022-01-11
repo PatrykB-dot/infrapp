0f5dfffb18c1b1b7d524cd90217423d59959f39b
PatrykB-dot <patrykfor3@gmail.com>
2022-01-11 09:21:54 +0100

    Update test_routes

--- app/__init__.py
+++ app/__init__.py
@@ -21,13 +21,11 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_pyfile('config.py')
-
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
     else:
         pass
-
     # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
