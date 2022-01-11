f64d60e52dda75e379f8cb1447d5329af74e5850
PatrykB-dot <patrykfor3@gmail.com>
2022-01-11 19:05:11 +0100

    Deleted test_routes

--- app/__init__.py
+++ app/__init__.py
@@ -15,21 +15,4 @@ if __name__ == "__main__":
 login = LoginManager(app)
 login.login_view = 'login'
 
-#from app import routes,models
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_pyfile('config.py')
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        pass
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    return app
